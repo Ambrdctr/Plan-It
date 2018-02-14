94894d65f2cf3489c742198734286b333c5adbbe
Gitravail <raphael.tournafond@gmail.com>
2018-02-14 14:10:33 +0100

    Ajout d'evenements
    Formulaires d'ajouts d'event

---
+++ Plan-it_Paul_Raph/index.php
@@ -0,0 +1,15 @@
+<?php
+	// Model
+	include_once "model/bdd.php";
+	include_once "model/afficher.php";
+	include_once "model/ajouts.php";
+
+	session_start();
+
+	// Controller
+	include_once "controller/action.php";
+	
+	// View
+	include_once "view/header.php";
+	include_once "view/footer.php";
+	
