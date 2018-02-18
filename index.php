bcd33947c72ee3a69dad5a01e0378e65ebbaaafc
Gitravail <raphael.tournafond@gmail.com>
2018-02-18 16:17:59 +0100

    Merge develop
    combinaison des deux parties

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	include_once "model/bdd.php";
 	include_once "model/afficher.php";
 	include_once "model/ajouts.php";
-
+	include_once "model/connexion.php";
 	session_start();
 
 	// Controller
--- view/ajout_event.php
+++ view/ajout_event.php
@@ -4,8 +4,9 @@ if (isset($_SESSION["error"])) {
 	unset($_SESSION["error"]);
 }
 ?>
-<form method="POST" action="." id="event">
-	<label for="id_type">Nom de l'evenement <span class="required">*</span> </label>
+<p class="required" id="js_error"></p>
+<form method="POST" action="." id="event" onsubmit="return verifAddEventForm(this)">
+	<label for="id_type">Nom de l'evenement<sup class='required'>*</sup> </label>
 	<select name="type" form="event" id="id_type" required autofocus>
 		<option value="" selected disabled hidden></option>
 		<?php
@@ -17,22 +18,22 @@ if (isset($_SESSION["error"])) {
 	</select>
 
 	<label for="id_desc">Description de l'evenement </label>
-	<textarea name="desc" id="id_desc" placeholder="Description"></textarea>
+	<textarea name="desc" id="id_desc" placeholder="Description" onblur="verifText(this)"></textarea>
 
 	<p>Debut</p>
-	<label for="id_Ddebut">Date <span class="required">*</span> </label>
+	<label for="id_Ddebut">Date<sup class='required'>*</sup> </label>
 	<input type="date" name="Ddebut" id="id_Ddebut" required="true" />
 	<label for="id_Hdebut">Heure </label>
 	<input type="time" name="Hdebut" id="id_Hdebut" />
 
 	<p>Fin</p>
-	<label for="id_Dfin">Date <span class="required">*</span> </label>
+	<label for="id_Dfin">Date<sup class='required'>*</sup> </label>
 	<input type="date" name="Dfin" id="id_Dfin" required="true" />
 	<label for="id_Hfin">Heure </label>
 	<input type="time" name="Hfin" id="id_Hfin" />
 
 	<label for="id_lieu">Lieu </label>
-	<input type="text" name="lieu" id="id_lieu"/>
+	<input type="text" name="lieu" id="id_lieu" onblur="verifText(this)"/>
 
 	<label for="id_prio">Prioritaire </label>
 	<input type="checkbox" name="prio" id="id_prio" />
@@ -42,3 +43,5 @@ if (isset($_SESSION["error"])) {
 
 	<input type="submit" name="action" value="Ajouter"/>
 </form>
+
+<p><sup class='required'>*</sup> : Champs obligatoires</p>
